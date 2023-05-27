a1697722f467038aec772a2bfa8b28e287e55aaa
Mr Zoom <mr.zoom.orsk@gmail.com>
2023-05-27 18:40:46 +0500

    Release 1.0(without design)

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QAbstractItemView, QPushButton, QDialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem, \
+    QVBoxLayout, QWidget, QAbstractItemView, QPushButton, QDialog
 from PyQt5 import uic
 from PyQt5.QtGui import QColor
 import sys
 import sqlite3
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 
 class MainWindow(QMainWindow):
@@ -15,7 +17,7 @@ class MainWindow(QMainWindow):
         self.con = sqlite3.connect('base.db')
         self.cur = self.con.cursor()
 
-        #connecting button
+        # connecting button
         self.btn_new_seizure.clicked.connect(self.open_dialog_new_seizure)
 
         # settings for table and button
@@ -28,6 +30,9 @@ class MainWindow(QMainWindow):
 
         # Connecting combobox
         self.table_filter.currentTextChanged.connect(self.fill_table)
+
+        # Connecting saving graphs
+        self.create_graph.clicked.connect(self.save_graphs)
 
     def fill_table(self):
         self.table.clearContents()
@@ -68,7 +73,9 @@ class MainWindow(QMainWindow):
         date = self.form_new_seizure.date_time.date().toPyDate()
         try:
             duration = int(duration)
-            self.cur.execute('INSERT INTO Seizures(duration, strength, symptomatic, reason, date) VALUES(?, ?, ?, ?, ?)', (duration, strength, symptomatic, reason, date))
+            self.cur.execute(
+                'INSERT INTO Seizures(duration, strength, symptomatic, reason, date) VALUES(?, ?, ?, ?, ?)',
+                (duration, strength, symptomatic, reason, date))
             self.con.commit()
             self.fill_table()
         except ValueError:
@@ -83,7 +90,8 @@ class MainWindow(QMainWindow):
             self.form_new_seizure.strength.setText(self.table.item(item.row(), 2).text())
             self.form_new_seizure.symptomatic.setText(self.table.item(item.row(), 3).text())
             self.form_new_seizure.reason.setText(self.table.item(item.row(), 4).text())
-            self.form_new_seizure.date_time.setDate(datetime.strptime(self.table.item(item.row(), 5).text(), '%Y-%m-%d'))
+            self.form_new_seizure.date_time.setDate(
+                datetime.strptime(self.table.item(item.row(), 5).text(), '%Y-%m-%d'))
             self.form_new_seizure.buttonBox.accepted.connect(self.refactor_seizure)
             self.form_new_seizure.show()
         elif item.text() == 'Удалить':
@@ -104,11 +112,52 @@ class MainWindow(QMainWindow):
         date = self.form_new_seizure.date_time.date().toPyDate()
         try:
             duration = int(duration)
-            self.cur.execute('UPDATE Seizures SET duration=?, strength=?, symptomatic=?, reason=?, date=? WHERE id=?', (duration, strength, symptomatic, reason, date, self.last_id))
+            self.cur.execute(
+                'UPDATE Seizures SET duration=?, strength=?, symptomatic=?, reason=?, date=? WHERE id=?',
+                (duration, strength, symptomatic, reason, date, self.last_id))
             self.con.commit()
             self.fill_table()
         except ValueError:
             pass
+
+    def save_graphs(self):
+        every = self.cur.execute('SELECT * FROM Seizures ORDER BY date').fetchall()
+        if len(every):
+            dates = [every[-1][5]]
+            values_frequency = [1]
+            values_duration = []
+            cur = every[-1][1]
+            length = 1
+            itog = 7
+            i = len(every) - 2
+            while itog:
+                if every[i][5] == dates[-1]:
+                    values_frequency[-1] += 1
+                    cur += every[i][1]
+                    length += 1
+                else:
+                    dates.append(every[i][5])
+                    values_frequency.append(1)
+                    values_duration.append(round(cur / length, 1))
+                    cur = every[i][1]
+                    length = 1
+                    itog -= 1
+                i -= 1
+                if i < 0:
+                    break
+            values_duration.append(round(cur / length, 1))
+            dates = dates[::-1]
+            values_frequency = values_frequency[::-1]
+            values_duration = values_duration[::-1]
+            plt.clf()
+            plt.plot(dates, values_frequency)
+            plt.title('Частота')
+            plt.savefig('saved_figure_frequency.png')
+            plt.clf()
+            plt.plot(dates, values_duration)
+            plt.title('Продолжительность(в мин)')
+            plt.savefig('saved_figure_duration.png')
+
 
 class DialogNewSeizure(QDialog):
     def __init__(self):
@@ -120,6 +169,7 @@ class DialogDeleteSeizure(QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi('dialog_delete_seizure.ui', self)
+
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
